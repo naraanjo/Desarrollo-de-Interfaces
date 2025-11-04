837d2e57576e30f11619c8f6d90b684421db0e29
naraanjo <185625172+naraanjo@users.noreply.github.com>
2025-11-04 07:44:40 +0100

    Actualización con contenido inicial

---
+++ Diseño de interfaces/UD2/EjerciciosContenedores/Grid-3/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Grid_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 3; i++) {
+
+                for (int j = 0; j < 3; j++) { 
+                    Button boton = new Button();
+
+                    boton.Width = 100;
+                    boton.Height = 50;
+                    boton.Content = "Boton " + i + ", " + j;
+
+                    boton.HorizontalAlignment = HorizontalAlignment.Left;
+                    boton.VerticalAlignment = VerticalAlignment.Top;
+
+                    Grid.SetRow(boton, i);
+                    Grid.SetColumn(boton, j);
+                    contenedor.Children.Add(boton);
+                   
+                }
+            }
+        }
+    }
+}
---
+++ Diseño de interfaces/UD2/EjerciciosContenedores/UniformGrid/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UniformGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            String[] opciones =  {"7","8", "9", "6", "5", "4", "3", "2", "1", "=","0" ,"C" };
+            int contador = 0;
+            InitializeComponent();
+
+            for (int i = 0; i < 4; i++) {
+
+                for (int j = 0; j < 3; j++) {
+                    Button boton = new Button();
+
+                    boton.Width = 50;
+                    boton.Height = 50;
+                    contenedor.Children.Add(boton);
+                    boton.Content = opciones[contador];
+                    contador++;                
+                }
+            }
+        }
+    }
+}
