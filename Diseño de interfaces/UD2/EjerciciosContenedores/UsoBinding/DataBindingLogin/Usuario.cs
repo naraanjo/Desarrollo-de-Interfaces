0bfd735552aa60c5c908c45fa50238b1c9d70458
naraanjo <185625172+naraanjo@users.noreply.github.com>
2025-11-05 19:32:46 +0100

    EjercicioEspejo

---
+++ Diseño de interfaces/UD2/EjerciciosContenedores/UsoBinding/DataBindingLogin/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32._Rellenado_ComboBox
+{
+    class Usuario
+    {
+        public string Login { get; set; } // Implementamos métodos getter y setter
+        public string Passw { get; set; }
+
+        // Constructor
+        public Usuario(String Login, String Passw)
+        {
+            this.Login = Login;
+            this.Passw = Passw;
+        }
+
+        // Sobrecargamos el método ToString
+        public override string ToString()
+        {
+            return Login + " " + Passw;
+        }
+
+        // Metodo para añadir usuario al desplegable
+
+    }
+}
